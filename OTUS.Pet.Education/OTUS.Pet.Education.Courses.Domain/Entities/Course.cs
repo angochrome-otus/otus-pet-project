0ffc16ec821fe5a2ec96164dbc0e99c807d57a65
Alex <templetters@mail.ru>
2025-11-26 17:56:23 +0300

    {ДОБАВЛЕНО} Старт работы над кодом проекта

---
+++ OTUS.Pet.Education/OTUS.Pet.Education.Courses.Domain/Entities/Course.cs
@@ -0,0 +1,32 @@
+﻿using OTUS.Pet.Education.Courses.Domain.Common;
+
+namespace OTUS.Pet.Education.Courses.Domain.Entities;
+
+/// <summary>
+/// Курс
+/// </summary>
+public class Course : Entity
+{
+    /// <summary>
+    /// Наименование курса
+    /// </summary>
+    public string Name { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Дата начала курса
+    /// </summary>
+    public DateOnly StartDate { get; set; }
+    
+    /// <summary>
+    /// Дата завершения курса
+    /// </summary>
+    public DateOnly EndDate { get; set; }
+   
+    //  Lessons(list)
+    public List<User> Students { get; set; } = new List<User>();
+
+    /// <summary>
+    /// Описание курса
+    /// </summary>
+    public string Subject { get; set; } = string.Empty;
+}
